540cbbfa51c6ee3c394858df529bd4d4b4f84600
KTachibanaM <whj19931115@gmail.com>
2024-02-25 23:29:19 +0000

    able to adjust polling interval minutes (for bsky mainly)

--- run.sh
+++ run.sh
@@ -4,8 +4,14 @@
 printenv | sed 's/^\([a-zA-Z0-9_]*\)=\(.*\)$/export \1="\2"/g' > /app/env.sh
 chmod +x /app/env.sh
 
-# Add a cron job to run main.py every 5 minutes
-echo "*/5 * * * * . /app/env.sh; /app/main.sh >> /var/log/cron.log 2>&1" | crontab -
+# Add a cron job to run main.py
+if [ -z "${INTERVAL_MINUTES}" ]; then
+    echo "INTERVAL_MINUTES environment variable is not available, using 5 minutes as interval by default"
+    echo "*/5 * * * * . /app/env.sh; /app/main.sh >> /var/log/cron.log 2>&1" | crontab -
+else
+    echo "INTERVAL_MINUTES environment variable is available, using ${INTERVAL_MINUTES} minutes as interval"
+    echo "*/${INTERVAL_MINUTES} * * * * . /app/env.sh; /app/main.sh >> /var/log/cron.log 2>&1" | crontab -
+fi
 
 # Create the log file to be able to run tail
 touch /var/log/cron.log
